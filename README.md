287747bc4601f02dce3c273e3500a0630a684e69
Henry Xiao <xiaohenry08@gmail.com>
2016-01-02 03:32:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###### UC San Diego Computer Science & Engineering Society
+
+Currently under construction. Estimated launch date: January, 2016
---
+++ _site/js/main.js
@@ -0,0 +1,99 @@
+$(document).on('ready', function() {
+
+    var wrapper = $('.wrapper');
+    wrapper.animate({
+        opacity: 1,
+        marginTop: 0
+    }, 700);
+
+    // Cache selectors
+    var lastId;
+    var nav = $('.nav-right-collapse, .nav-right');
+    var navHeight = 60;
+    // all list items
+    var links = nav.find("a");
+    // anchors corresponding to menu items
+    var scrollLinks = links.map(function() {
+      var link = $($(this).attr("href"));
+      if (link.length) { return link; }
+    });
+
+    // scroll animation
+    links.on('click', function(e){
+      var href = $(this).attr("href");
+      var offsetTop = (href === "#") ? 0 : $(href).offset().top-navHeight+10;
+      $('html, body').stop().animate({
+          scrollTop: offsetTop
+      }, 300);
+      e.preventDefault();
+    });
+
+
+    $(document).on('scroll', function() {
+        var scrollTop = $(document).scrollTop();
+        if (scrollTop > 60) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+
+    // Bind to scroll
+    $(document).on('scroll', function() {
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+navHeight+25;
+
+       // Get id of current scroll item
+       var cur = scrollLinks.map(function() {
+         if ($(this).offset().top < fromTop) {
+           return this;
+       }
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+       if (lastId !== id) {
+           lastId = id;
+            links.removeClass('active');
+            links.filter("[href=#"+id+"]").addClass('active');
+         }
+     });
+
+     $('.contact-submit-btn').on('click', function() {
+         $(this).submit();
+     })
+
+     // esc hit - close form or close nav-collapse
+     $(document).keyup(function(e) {
+ 		if (e.keyCode === 27) { // escape key maps to keycode `27`
+ 			if ($('.subscribe-form').css('display') === 'block') {
+                 $('.subscribe-close').click();
+             } else {
+                 $('.nav-right-hamburger, .nav-right-collapse').removeClass('on');
+             }
+ 		}
+ 	});
+
+     // subscribe clicked - show subscribe form
+     $('.subscribe-btn').on('click', function() {
+         $('.subscribe-overlay').show();
+         $('.subscribe-form').show().animate({
+             top: '30%'
+         }, 200);
+     });
+
+     // subscribe-close or outside of form is clicked - exit form
+     $('.subscribe-overlay, .subscribe-close').on('click', function() {
+         $('.subscribe-overlay').hide();
+         $('.subscribe-form').css('top','28%');
+         $('.subscribe-form').hide();
+     });
+
+     // hamburger nav clicked - toggle nav-collapse
+     $('.nav-right-hamburger, .nav-right-collapse a').on('click', function() {
+         $('.nav-right-hamburger').toggleClass('on');
+         $('.nav-right-collapse').toggleClass('on');
+     });
+
+
+});
